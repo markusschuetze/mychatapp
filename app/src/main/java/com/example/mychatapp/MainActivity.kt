10ed469f96b84a6269453c4220cc0e7ef193ce73
markusschuetze <markus.schuetze@smail.th-koeln.de>
2023-01-09 21:59:54 +0100

    Projekt erstellt, Firebase eingerichtet

---
+++ app/src/main/java/com/example/mychatapp/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.mychatapp.com.example.mychatapp;
+
+import android.content.Context
+import android.widget.Button
+import android.app.Activity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+
